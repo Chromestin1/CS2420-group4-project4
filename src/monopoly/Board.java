2b2f3de9236222a913551c843266f787ee3864c4
Chromestin1 <83986035+Chromestin1@users.noreply.github.com>
2026-04-14 12:07:28 -0600

    Update Board.java

--- src/monopoly/Board.java
+++ src/monopoly/Board.java
@@ -2,6 +2,23 @@ package monopoly;
 
 public class Board {
 
+	private String name;
+	private int totalMoves;
+	
+	/*
+	 * TODO
+	 */
+	public void discardPile() {
+		
+	}
+	
+	/*
+	 * TODO
+	 */
+	public void reshuffle() {
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
