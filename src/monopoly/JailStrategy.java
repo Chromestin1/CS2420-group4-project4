e182c429b72d952cc686d4f8a64e22437e3d3bde
Chromestin1 <83986035+Chromestin1@users.noreply.github.com>
2026-04-20 12:13:18 -0600

    Update JailStrategy.java

--- src/monopoly/JailStrategy.java
+++ src/monopoly/JailStrategy.java
@@ -1,25 +1,67 @@
 package monopoly;
+	/**
+	 * Handles the two different strategies for getting out of jail in game play
+	 * 
+	 * @author KatM
+	 */
 
-public class JailStrategy {
+	//TurnEngine must implement these for this class to work: getJailCardCount(), setInJail(), useJailCard(), isDoubles(), rollDice()
+	public class JailStrategy {
 
-	/*
-	 * TODO
-	 */
-	public void StrategyA(TurnEngine engine) {
+		/*
+		 * TODO
+		 */
+		private int doublesAttempts;
+
+		/**
+		 * If the player has a get out jail free card, use it. Otherwise assume paid $50
+		 * and exit
+		 * 
+		 * @param engine
+		 */
 		
-	}
-	
-	/*
-	 * TODO
-	 */
-	public void StrategyB(TurnEngine engine) {
+		public void strategyA(TurnEngine engine) {
+
+			if (engine.getJailCardCount() > 0) {
+				engine.setInJail(false);
+				engine.useJailCard(); // a method that should return the card to Deck
+			} else
+				engine.setInJail(false);
+			// player still needs to take their turn after exiting jail - handled by
+			// TurnEngine?
+		}
 		
-	}
-	
-	/*
-	 * TODO
-	 */
-	public String getName() {
-		return null;
-	}
+		/*
+		 * TODO
+
+		/**
+		 * If the player has a get out jail free card, use it. Otherwise must roll
+		 * doubles to get out If after 3 rolls there are no doubles, exit.
+		 * 
+		 * @param engine
+		 */
+		
+		public void strategyB(TurnEngine engine) {
+
+			if (engine.getJailCardCount() > 0) {
+				engine.setInJail(false);
+				engine.useJailCard(); // a method that should return the card to Deck
+			} else {
+				// player exits after rolling doubles OR after 3 failed attempts (exits on 4th
+				// turn)
+				while (!engine.isDoubles() && doublesAttempts < 3) {
+					engine.rollDice();
+					doublesAttempts++;
+				}
+				engine.setInJail(false);
+				doublesAttempts = 0;
+
+			}
+
+		}
+		
+
+		/*
+		 * TODO
+		 */
 }
