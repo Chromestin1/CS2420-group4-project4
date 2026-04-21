242b87685beae0dec996cffaa6f6b5d609e3fc2e
Chromestin1 <83986035+Chromestin1@users.noreply.github.com>
2026-04-20 20:47:51 -0600

    Update Deck.java
    The shuffle method has been implemented. It has a helper method to go along with it.

--- src/monopoly/Deck.java
+++ src/monopoly/Deck.java
@@ -7,16 +7,32 @@ public class Deck {
 	private Stack<String> commChestCards;
 	
 	public String drawChance() {
+		//TODO
 		return null;
 		
 	}
 	
 	public String drawCommChest() {
+		//TODO
 		return null;
 		
 	}
 	
-	public void shuffle() {
-		
+	public void shuffle(String[] a) {
+		int n = a.length;
+		for(int i = 0; i < n; i++) {
+			int r = i + (int) (Math.random() * (n-i));
+			exch(a, i, r);
+		}
 	}
+	
+	/*
+	 * helper method for shuffle method
+	 */
+	public static void exch(String[] a, int i, int j) {
+		String swap = a[i];
+		a[i] = a[j];
+		a[j] = swap;
+	}
+	
 }
